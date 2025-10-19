4aa1f4883c4bb1c204f24803620e1a6f84b7dfcc
Krongphop <Krongphop10@gmail.com>
2025-10-19 17:40:16 +0700

    Initial commit

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\TeacherController;
+use App\Http\Controllers\StudentController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -29,18 +31,18 @@ Route::middleware(['auth'])->group(function () {
     Route::put('/users/{id}', [UserController::class, 'update'])->name('users.update');
     
     // Teachers
-    Route::get('/teachers', [\App\Http\Controllers\TeacherController::class, 'index'])->name('teachers.index');
-    Route::get('/teachers/create', [\App\Http\Controllers\TeacherController::class, 'create'])->name('teachers.create');
-    Route::post('/teachers', [\App\Http\Controllers\TeacherController::class, 'store'])->name('teachers.store');
-    Route::get('/teachers/{id}', [\App\Http\Controllers\TeacherController::class, 'show'])->name('teachers.show');
-    Route::get('/teachers/{id}/edit', [\App\Http\Controllers\TeacherController::class, 'edit'])->name('teachers.edit');
-    Route::put('/teachers/{id}', [\App\Http\Controllers\TeacherController::class, 'update'])->name('teachers.update');
+    Route::get('/teachers', [TeacherController::class, 'index'])->name('teachers.index');
+    Route::get('/teachers/create', [TeacherController::class, 'create'])->name('teachers.create');
+    Route::post('/teachers', [TeacherController::class, 'store'])->name('teachers.store');
+    Route::get('/teachers/{id}', [TeacherController::class, 'show'])->name('teachers.show');
+    Route::get('/teachers/{id}/edit', [TeacherController::class, 'edit'])->name('teachers.edit');
+    Route::put('/teachers/{id}', [TeacherController::class, 'update'])->name('teachers.update');
 
     // Students
-    Route::get('/students', [\App\Http\Controllers\StudentController::class, 'index'])->name('students.index');
-    Route::get('/students/create', [\App\Http\Controllers\StudentController::class, 'create'])->name('students.create');
-    Route::post('/students', [\App\Http\Controllers\StudentController::class, 'store'])->name('students.store');
-    Route::get('/students/{id}', [\App\Http\Controllers\StudentController::class, 'show'])->name('students.show');
-    Route::get('/students/{id}/edit', [\App\Http\Controllers\StudentController::class, 'edit'])->name('students.edit');
-    Route::put('/students/{id}', [\App\Http\Controllers\StudentController::class, 'update'])->name('students.update');
+    Route::get('/students', [StudentController::class, 'index'])->name('students.index');
+    Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
+    Route::post('/students', [StudentController::class, 'store'])->name('students.store');
+    Route::get('/students/{id}', [StudentController::class, 'show'])->name('students.show');
+    Route::get('/students/{id}/edit', [StudentController::class, 'edit'])->name('students.edit');
+    Route::put('/students/{id}', [StudentController::class, 'update'])->name('students.update');
 });
